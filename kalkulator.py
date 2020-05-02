582b95248e6cccce65ccf6087617cf70841b0c8b
Filip Gabrys <el.filberto@gmail.com>
2020-05-02 10:59:08 +0200

    Add calculator base function

--- kalkulator.py
+++ kalkulator.py
@@ -1,1 +1,20 @@
-#kod
+import logging
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+def calculate(task, numbers):
+    #dodawanie
+    if task == 1:
+        result = a+b
+        return result
+    #odejmowanie
+    elif task == 2:
+        result = a-b
+        return result
+    #mnożenie
+    elif task == 3:
+        result = a*b
+        return result
+    #dzielenie
+    elif task == 4:
+        result = a/b
+        return result
