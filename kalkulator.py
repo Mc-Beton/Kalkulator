50d5c335176f94c4694cb870ef74c4cbd4ead19a
Filip Gabrys <el.filberto@gmail.com>
2020-05-02 12:08:22 +0200

    Upgrade the sum and multiply function

--- kalkulator.py
+++ kalkulator.py
@@ -1,37 +1,66 @@
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
+
+#Funkcja odejmowania i dzielenia
 def calculate(task, a, b):
-    #result=""
-    #result = result + "Wynik działania nr %d.\n" % task
-    #dodawanie
-    if task == "1":
-        suma = a+b
-        print(f"Dodaję {a} i {b}")
-        return f"Wynik to {suma}"
     #odejmowanie
-    elif task == "2":
+    if task == "2":
         result = a-b
         print(f"Odejmuję {a} od {b}")
         return f"Wynik to {result}"
-    #mnożenie
-    elif task == "3":
-        result = a*b
-        print(f"Mnożę {a} i {b}")
-        return f"Wynik to {result}"
+
     #dzielenie
     elif task == "4":
         result = a/b
         print(f"Dzielę {a} przez {b}")
         return f"Wynik to {result}"
 
+#Funkcja dodawania i mnożenia
+def calculate2(task, a):
+    #dodawanie
+    if task == "1":
+        result = 0
+        for ele in a:
+            result = result + ele
+        return f"Suma wszystkich liczb to {result}"
+    #mnożenie
+    elif task == "3":
+        result=1
+        for ele in a:
+            result = result * ele
+        return f"wynik mnożenia wszystkich liczb to {result}"
+
+#funkcja wyboru działania
+def what_task(task):
+    if task == "1":
+        print("Podaj liczby jakie chcesz dodać kolejno je zatwierdzając i zakończ wpisaniem stop")
+        try: 
+            a = [] 
+            while True: 
+                a.append(float(input())) 
+        except: 
+            print(calculate2(task, a))
+    elif task == "2":
+        a = float(input("Podaj pierwszą liczbę "))
+        b = float(input("Podaj drugą liczbę "))
+        print(calculate(task, a, b))
+    elif task == "3":
+        print("Podaj liczby jakie chcesz pomnożyć ze sobą kolejno je zatwierdzając i zakończ wpisaniem stop")
+        try: 
+            a = [] 
+            while True: 
+                a.append(float(input())) 
+        except: 
+            print(calculate2(task, a))
+    elif task == "4":
+        a = float(input("Podaj pierwszą liczbę "))
+        b = float(input("Podaj drugą liczbę "))
+        print(calculate(task, a, b))
+
 if __name__ == "__main__":
     task = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie ")
-    a = input("Podaj pierwszą liczbę ")
-    b = input("Podaj drugą liczbę ")
-    a=int(a)
-    b=int(b)
-    print(calculate(task, a, b))
+    what_task(task)
     
     
 
