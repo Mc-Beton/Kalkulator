7f17fc18f7b782aa979cf07f71b8c018a9119314
Filip Gabrys <el.filberto@gmail.com>
2020-05-02 11:08:27 +0200

    Add open program

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,7 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
+#logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
 
-def calculate(task, numbers):
+def calculate(task, a, b):
     #dodawanie
     if task == 1:
         result = a+b
@@ -18,3 +18,10 @@ def calculate(task, numbers):
     elif task == 4:
         result = a/b
         return result
+
+if __name__ == "__main__":
+    task = input("Podaj działanie, posługując się odpowiednią liczbą: 1 Dodawanie, 2 Odejmowanie, 3 Mnożenie, 4 Dzielenie")
+    a = input("Podaj pierwszą liczbę")
+    b = input("Podaj drugą liczbę")
+    calc_result = calculate(task, a, b)
+    print(calc_result)
